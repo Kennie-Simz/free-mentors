1fbc8927c0eb29989ac5258caa28351a7ba7f15b
Kennedy Nyongesa <nyongesa17@outlook.com>
2019-08-18 00:34:15 +0300

    working on homepage modifications

--- UI/home.js
+++ UI/home.js
@@ -25,16 +25,3 @@ const navSlide = () => {
 
 navSlide();
 
-/*const searchBar = document.forms['search'].querySelector('input');
-searchBar.addEventListener('keyup', (e) => {
-    const term = e.target.value.toLowerCase();
-    const mentors = list.getElementsByTagName('li');
-    Array.from(mentors).forEach(mentor) => {
-        const title = mentor.firstElementChild.textContent;
-        if (title.toLowerCase().indexOf(term)!= -1){
-            mentor.style.display = 'block';
-        } else {
-            mentor.style.display = none;
-        }
-    }
-}) */
