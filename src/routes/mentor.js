f71f01af46418859462d8a6aef696e97c845b96c
Kennedy Nyongesa <nyongesa17@outlook.com>
2019-09-06 05:49:22 +0300

    [Finishes #168333572] Errors fixed

--- src/routes/mentor.js
+++ src/routes/mentor.js
@@ -3,8 +3,6 @@ import mentorsController from '../controllers/mentorsController';
 
 const mentorRouter = express.Router();
 
-mentorRouter.post('/signup', mentorsController.signUpMentor);
-mentorRouter.post('/:mentorId', mentorsController.logMentor);
 mentorRouter.get('/', mentorsController.allMentors);
 mentorRouter.get('/:mentorId', mentorsController.getSingleMentor);
 
