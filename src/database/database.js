1257bb8d4f24eec5b58b2c525c0036009cd1afbc
Kennedy Nyongesa <nyongesa17@outlook.com>
2019-09-10 14:36:34 +0300

    --amend

--- src/database/database.js
+++ src/database/database.js
@@ -19,35 +19,8 @@ import pool from './index';
      createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP
    )`;
 
-  const mentors = `CREATE TABLE IF NOT EXISTS mentors (
-     id serial PRIMARY KEY,
-     firstName VARCHAR(255) NOT NULL,
-     lastName  VARCHAR(255) NOT NULL,
-     email  VARCHAR(255) NOT NULL UNIQUE,
-     password  VARCHAR(255) NOT NULL,
-     address  VARCHAR(255) NOT NULL,
-     bio  VARCHAR(255) NOT NULL,
-     occupation  VARCHAR(255) NOT NULL,
-     expertise  VARCHAR(255) NOT NULL,
-     isAdmin boolean DEFAULT false,
-     createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-   )`;
-  const session = `CREATE TABLE IF NOT EXISTS mentors (
-     id serial PRIMARY KEY,
-     mentorId VARCHAR(255) NOT NULL UNIQUE,
-     menteeId  VARCHAR(255) NOT NULL UNIQUE,
-     questions  VARCHAR(255) NOT NULL,
-     menteeEmail  VARCHAR(255) NOT NULL UNIQUE,
-     status  VARCHAR(255) NOT NULL,
-     createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-   )`;
-
   await pool.query(dropTables);
   console.log('Dropping tables');
   await pool.query(users);
   console.log('users created');
-  await pool.query(mentors);
-  console.log('upgraded to mentor!');
-  await pool.query(session);
-  console.log('session created!');
 })();
